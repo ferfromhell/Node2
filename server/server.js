952392684166581666c905f16af2c0d4891bb1e1
Admin <admin@FerfromHell.local>
2018-10-05 11:13:59 -0700

    Chapter 3

--- server/server.js
+++ server/server.js
@@ -1,17 +1,36 @@
-
-
 var express = require('express');
 var bodyParser = require('body-parser');
 
-const mongoose = require ('./db/mongoose');
-var {Todo} = require ('./model/serverTodo.js');
-var  {User} = require ('./model/serverUser.js');
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./model/todo');
+var {User} = require('./model/user');
 
 var app = express();
 
-app.listen(3000, ()=>{
-    console.log('Started on port 3000');
-});
 app.use(bodyParser.json());
- 
-//app.post(''):
+
+app.post('/todos', (req, res) => {
+  var todo = new Todo({
+         text: req.body.text
+   });
+
+   todo.save().then((doc) => {
+     res.send(doc);
+   }, (e) => {
+     res.status(400).send(e);
+   });
+});
+
+app.get('/todos', (req, res) => {
+   Todo.find().then((todos) => {
+         res.send({todos});
+   }, (e) => {
+         res.status(400).send(e);
+   });
+});
+
+app.listen(3000, () => {
+   console.log('Started on port 3000');
+});
+
+module.exports = {app};
